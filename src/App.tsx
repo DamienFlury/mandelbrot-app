402274678e9ab5612d431e9af83dbeae35f95caf
Damien Flury <damien.flury@gmail.com>
2020-02-04 15:45:41 +0100

    Swap re/im parts

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,39 @@ import { generateMandelbrot } from './functions/mandelbrot';
 
 const App = () => {
   const ref = useRef<HTMLCanvasElement | null>(null);
-  const width = 400;
-  const height = 400;
-  const mandelbrotSet = generateMandelbrot({ width, height, reRange: [-1.5, 1.5], imRange: [-1.5, 1.5]});
+  const width = 500;
+  const height = 500;
+  const mandelbrotSet = generateMandelbrot({
+    width,
+    height,
+    reRange: [-1.75, 0.75],
+    imRange: [-1.25, 1.25],
+  });
+  console.log('Mandelbrot generated... Drawing...')
   // console.log(mandelbrotSet);
   useEffect(() => {
     const canv = ref.current;
-    if(!canv) { return; }
+    if (!canv) {
+      return;
+    }
     const context = canv.getContext('2d');
-    if(!context) { return; }
+    if (!context) {
+      return;
+    }
     const imageData = context.createImageData(width, height);
-    imageData.data.set(mandelbrotSet.flatMap(x => x ? [0, 0, 0, 255] : [255, 255, 255, 255]));
+    imageData.data.set(
+      mandelbrotSet.flatMap(x => (x ? [0, 0, 0, 255] : [255, 255, 255, 255]))
+    );
     context.putImageData(imageData, 5, 5);
-    // for(let i = 0; i < width; i += 5) {
-    //   for(let j = 0; j < height; j += 5) {
-
-    //     // console.log(i * width + j)
-    //     if(mandelbrotSet[i * width + j]) {
-    //       context.rect(j, i, 5, 5);
-    //       context.fill();
-    //     }
-    //   }
-    // }
   }, [mandelbrotSet]);
   return (
-    <canvas width={width} height={height} style={{background: 'white'}} ref={ref} />
+    <canvas
+      width={width}
+      height={height}
+      style={{ background: 'white' }}
+      ref={ref}
+    />
   );
-}
+};
 
 export default App;
--- src/functions/mandelbrot.ts
+++ src/functions/mandelbrot.ts
@@ -3,8 +3,8 @@ import * as math from 'mathjs';
 type Props = {
   width: number;
   height: number;
-  reRange: NumberTuple,
-  imRange: NumberTuple,
+  reRange: NumberTuple;
+  imRange: NumberTuple;
 };
 
 const diverges = (num: math.Complex) => {
@@ -33,10 +33,10 @@ const map = ({ x, origin: [min, max], target: [newMin, newMax] }: MapProps) =>
 const generateMandelbrot = ({ width, height, reRange, imRange }: Props) => {
   const mandelbrotSet: boolean[] = [];
 
-  for (let i = 0; i < width; i += 1) {
-    for (let j = 0; j < height; j += 1) {
-      const re = map({ x: i, origin: [0, width], target: reRange});
-      const im = map({ x: j, origin: [0, height], target: imRange});
+  for (let j = 0; j < height; j += 1) {
+    for (let i = 0; i < width; i += 1) {
+      const re = map({ x: i, origin: [0, width], target: reRange });
+      const im = map({ x: j, origin: [0, height], target: imRange });
 
       const z = math.complex(re, im);
       mandelbrotSet.push(diverges(z));
